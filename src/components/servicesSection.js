286bbe60968f8da6a42d027d83f980a3c784d387
Guido <carucciguido@gmail.com>
2019-11-22 14:36:15 +0100

    404 removed

--- src/components/servicesSection.js
+++ src/components/servicesSection.js
@@ -1,7 +1,20 @@
 import React from "react"
+import {useStaticQuery, graphql} from 'gatsby'
 import "./servicesSection.css"
+import Img from 'gatsby-image'
 
 export default () => {
+  const data = useStaticQuery(graphql`
+  query {
+    placeholderImage: file(relativePath: { eq: "monitor-sketch.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 128) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`)
   return (
     <section className="services-section">
       <div className="services-section-container">
@@ -15,6 +28,8 @@ export default () => {
         </p>
         <div className="services-container">
             <div>
+            <Img fluid={data.placeholderImage.childImageSharp.fluid}
+          />
                 <p className="service-title"><strong>WEB DEVELOPMENT</strong></p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                 <p><strong>> Find out more</strong></p>
