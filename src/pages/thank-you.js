8babddd60eedee6f0dbb5d09a372b0b2d9f1d3d7
Guido <carucciguido@gmail.com>
2019-11-26 14:38:56 +0100

    netifly form redirect

---
+++ src/pages/thank-you.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const thankYou = ({ pageContext: { locale }, data }) => {
+  
+    return(
+     
+      
+      <section>
+          <h1>thank you</h1>
+      </section>
+    
+    )
+  } 
+  export default thankYou
