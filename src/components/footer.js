4d5a78a199cd2f9d5f4f53d441c53989e474f1d4
Guido <carucciguido@gmail.com>
2020-01-10 13:06:50 +0100

    email fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,11 @@
-import React from "react"
-import "./footer.css"
-import { useStaticQuery, graphql, Link } from "gatsby"
-import Img from "gatsby-image"
-import LocalizedLink from "./LocalizedLink"
-import italyFlag from "../images/italy-flag.png"
-import ukFlag from "../images/uk-flag.png"
-import franceFlag from "../images/france-flag.png"
+import React from 'react'
+import './footer.css'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
+import LocalizedLink from './LocalizedLink'
+import italyFlag from '../images/italy-flag.png'
+import ukFlag from '../images/uk-flag.png'
+import franceFlag from '../images/france-flag.png'
 
 export default () => {
   const data = useStaticQuery(graphql`
@@ -70,92 +70,92 @@ export default () => {
   `)
 
   return (
-    <footer className="footer">
-      <div className="footer-container">
-        <div className="footer-section-container">
-          <LocalizedLink to="/">
+    <footer className='footer'>
+      <div className='footer-container'>
+        <div className='footer-section-container'>
+          <LocalizedLink to='/'>
             <Img
-              className="logo-footer"
+              className='logo-footer'
               fluid={data.logo.childImageSharp.fluid}
             />
           </LocalizedLink>
-          <div className="flags-container-footer">
-            <a href="https://vanillamarketing.it/">
-              <img className="flag-footer" src={italyFlag} alt="italy-flag" />
+          <div className='flags-container-footer'>
+            <a href='https://vanillamarketing.it/'>
+              <img className='flag-footer' src={italyFlag} alt='italy-flag' />
             </a>
-            <a href="https://vanillamarketing.it/en">
-              <img className="flag-footer" src={ukFlag} alt="uk-flag" />
+            <a href='https://vanillamarketing.it/en'>
+              <img className='flag-footer' src={ukFlag} alt='uk-flag' />
             </a>
-            <a href="https://vanillamarketing.it/fr">
-              <img className="flag-footer" src={franceFlag} alt="france-flag" />
+            <a href='https://vanillamarketing.it/fr'>
+              <img className='flag-footer' src={franceFlag} alt='france-flag' />
             </a>
           </div>
         </div>
-        <div className="footer-section-container">
-          <p className="footer-title">Services</p>
-          <LocalizedLink className="footer-link" to="/realizzazione-siti-web">
+        <div className='footer-section-container'>
+          <p className='footer-title'>Services</p>
+          <LocalizedLink className='footer-link' to='/realizzazione-siti-web'>
             Web Development
           </LocalizedLink>
-          <LocalizedLink className="footer-link" to="/social-media-marketing">
+          <LocalizedLink className='footer-link' to='/social-media-marketing'>
             Social Media
           </LocalizedLink>
-          <LocalizedLink className="footer-link" to="/web-marketing">
+          <LocalizedLink className='footer-link' to='/web-marketing'>
             Web Marketing
           </LocalizedLink>
-          <LocalizedLink className="footer-link" to="/graphic-design">
+          <LocalizedLink className='footer-link' to='/graphic-design'>
             Graphic Design
           </LocalizedLink>
         </div>
-        <div className="footer-section-container">
-          <p className="footer-title">Contact</p>
-          <div className="footer-contact-container">
+        <div className='footer-section-container'>
+          <p className='footer-title'>Contact</p>
+          <div className='footer-contact-container'>
             <Img
-              className="icon-footer"
+              className='icon-footer'
               fluid={data.mapIcon.childImageSharp.fluid}
             />
             <a
-              className="footer-link"
-              target="_blank"
-              href="https://www.google.com/maps/place/Corso+Giuseppe+Garibaldi,+91,+60121+Ancona+AN,+Italia/@43.6177941,13.5117425,17z/data=!3m1!4b1!4m5!3m4!1s0x132d7fbc3d6aac41:0x2f2b499114e7475b!8m2!3d43.6177902!4d13.5139312"
+              className='footer-link'
+              target='_blank'
+              href='https://www.google.com/maps/place/Corso+Giuseppe+Garibaldi,+91,+60121+Ancona+AN,+Italia/@43.6177941,13.5117425,17z/data=!3m1!4b1!4m5!3m4!1s0x132d7fbc3d6aac41:0x2f2b499114e7475b!8m2!3d43.6177902!4d13.5139312'
             >
               Corso Garibaldi 91/b, Ancona
             </a>
           </div>
-          <div className="footer-contact-container">
+          <div className='footer-contact-container'>
             <Img
-              className="icon-footer"
+              className='icon-footer'
               fluid={data.emailIcon.childImageSharp.fluid}
             />
             <a
-              className="footer-link"
-              target="_blank"
-              href="mailto:info@vanilla.marketing"
+              className='footer-link'
+              target='_blank'
+              href='mailto:info@vanillamarketing.it'
             >
-              info@vanilla.marketing
+              info@vanillamarketing.it
             </a>
           </div>
-          <div className="footer-contact-container">
+          <div className='footer-contact-container'>
             <Img
-              className="icon-footer"
+              className='icon-footer'
               fluid={data.phoneIcon.childImageSharp.fluid}
             />
-            <a className="footer-link" href="tel:+393516711311">
+            <a className='footer-link' href='tel:+393516711311'>
               +39 071 204002
             </a>
           </div>
-          <div className="footer-contact-container">
+          <div className='footer-contact-container'>
             <Img
-              className="icon-footer"
+              className='icon-footer'
               fluid={data.mobileIcon.childImageSharp.fluid}
             />
-            <a className="footer-link" href="tel:+39071204002">
+            <a className='footer-link' href='tel:+39071204002'>
               +39 351 671 1311
             </a>
           </div>
         </div>
-        <div className="footer-section-container">
-          <p className="footer-title">Social</p>
-          <div className="footer-social-container">
+        <div className='footer-section-container'>
+          <p className='footer-title'>Social</p>
+          <div className='footer-social-container'>
             {/* <a
               href="https://www.facebook.com/AgenziaVanillaMarketing"
               target="_blank"
@@ -193,29 +193,29 @@ export default () => {
               />
             </a> */}
             <a
-              className="footer-link"
-              href="https://www.facebook.com/AgenziaVanillaMarketing"
+              className='footer-link'
+              href='https://www.facebook.com/AgenziaVanillaMarketing'
               target='_blank'
             >
               Facebook
             </a>
             <a
-              className="footer-link"
-              href="https://www.instagram.com/vanilla.marketing/"
+              className='footer-link'
+              href='https://www.instagram.com/vanilla.marketing/'
               target='_blank'
             >
               Instagram
             </a>
             <a
-              className="footer-link"
-              href="https://www.linkedin.com/company/vanilla-marketing"
+              className='footer-link'
+              href='https://www.linkedin.com/company/vanilla-marketing'
               target='_blank'
             >
               Linkedin
             </a>
             <a
-              className="footer-link"
-              href="https://open.spotify.com/user/jq8rsmhffl01j9izazr9hx2yk?si=fJCM68DNRxuUxsYcWoay_g"
+              className='footer-link'
+              href='https://open.spotify.com/user/jq8rsmhffl01j9izazr9hx2yk?si=fJCM68DNRxuUxsYcWoay_g'
               target='_blank'
             >
               Spotify
