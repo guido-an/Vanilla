b0bd0beabbd6ac5a5d27e3b4ad4b7fb9b9daf9f0
Guido <carucciguido@gmail.com>
2020-05-05 20:05:51 +0200

    update prenotadamangiare

--- src/pages/prenota-da-mangiare.js
+++ src/pages/prenota-da-mangiare.js
@@ -1,6 +1,7 @@
 import './prenota-da-mangiare.css'
 import React from 'react'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import SEO from '../components/seo'
 import Layout from '../components/layout'
 import FooterCta from '../components/footerCta'
@@ -14,18 +15,19 @@ import hearthIcon from '../images/hearth-icon.jpg'
 const prenotaDaMangiare = ({ pageContext: { locale }, data }) => {
   return (
     <Layout path='/' locale={locale} data={data}>
-      <SEO title='Prenota Da Mangiare' description='Il Tuo Locale Online' />
+      <SEO title='Prenota Da Mangiare | Il Tuo Locale Online' />
       <section className='prenota-da-mangiare-section'>
         <div className='prenota-da-mangiare-hero'>
           <div>
-            <span className='overtitle-dark'>IL TUO LOCALE ONLINE</span>
+            <span className='overtitle-dark'>PORTA IL TUO LOCALE ONLINE CON</span>
             <h1>Prenota Da Mangiare</h1>
             <div>
               <div className='prenota-da-mangiare-list-item'><img src={checkMark} alt='check-mark' /><p>Fatto su misura per te</p></div>
               <div className='prenota-da-mangiare-list-item'><img src={checkMark} alt='check-mark' /><p>La nostra esperienza a tua disposizione</p></div>
               <div className='prenota-da-mangiare-list-item'><img src={checkMark} alt='check-mark' /><p>Super eroi della customer care</p></div>
             </div>
-            <p>Nessun costo di attivazione | Disponibilità immediata | Facile da usare</p>
+            <p>La gestione online di delivery e take away senza pensieri.<br />Ne vuoi sapere di più?</p>
+            {/* <p>Nessun costo di attivazione | Disponibilità immediata | Facile da usare</p> */}
             <p className='prenota-da-mangiare-cta-text'>Mettiti In Contatto Con Noi Ora <a href='https://wa.me/+393516711311'><img src={whatsapp} alt='whatsapp-icon' /></a></p>
           </div>
           <div>
@@ -34,30 +36,30 @@ const prenotaDaMangiare = ({ pageContext: { locale }, data }) => {
         </div>
         <div className='prenota-da-mangiare-testimonial'>
           <div className='prenota-da-mangiare-wrapper'>
-            <p><strong>Lorem ipsum<br />doloret sin amet:</strong></p>
+            <p><strong>Cosa ne pensa<br />chi lo sta già utilizzando?</strong></p>
             <p><span className='quotes'>"</span>Ho scelto prenotadamangiare.it e tutto è diventato più semplice! È facile da usare sia per me che per i miei clienti e questo significa avere più ordini e lavorare con più serenità! L'assistenza, poi, è molto preparata e disponibile. Sono assolutamente soddisfatta!</p>
           </div>
           <p className='testimonial-name'>Sofia, Zazie</p>
         </div>
         <div className='prenota-da-mangiare-values'>
-          <h2>Lorem ipsum doloret sin amet</h2>
+          <h2>Vendere Online Con Facilità</h2>
           <div className='divider' />
-          <p className='subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure nisi quod quidem eos aspernatur, dicta architecto consequatur cum nemo eius tenetur.</p>
+          <p className='subtitle'>La gestione di delivery e take away che funziona! Tutta l’esperienza della nostra digital agency a servizio del tuo locale, perché la giusta soluzione è facile.</p>
           <div className='icons-wrapper'>
-            <div>
+            <div style={{ margin: '0 20px ' }}>
               <img src={boatIcon} alt='boat-icon' />
-              <p><strong>Lorem Ipsum</strong></p>
-              <p className='values-subtitle'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita maiores asperiores magnam architecto iusto quod, quos optio modi exercitationem natus.</p>
+              <p><strong>Fatto su misura per te</strong></p>
+              <p className='values-subtitle'>Non tutte le esigenze sono uguali, questo è il motivo per cui ti proponiamo una soluzione fatta su misura: per non farti perdere tempo e per vederti soddisfatto.</p>
             </div>
-            <div>
+            <div style={{ margin: '0 20px ' }}>
               <img src={elephantIcon} alt='check-mark' />
-              <p><strong>Lorem Ipsum</strong></p>
-              <p className='values-subtitle'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita maiores asperiores magnam architecto iusto quod, quos optio modi exercitationem natus.</p>
+              <p><strong>La nostra esperienza a tua disposizione</strong></p>
+              <p className='values-subtitle'>È la nostra esperienza che ci ha fatto creare 'Prenota Da Mangiare': il servizio facile da usare e per questo davvero efficace per te e per i tuoi clienti.</p>
             </div>
-            <div>
+            <div style={{ margin: '0 20px ' }}>
               <img src={hearthIcon} alt='check-mark' />
-              <p><strong>Lorem Ipsum</strong></p>
-              <p className='values-subtitle'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita maiores asperiores magnam architecto iusto quod, quos optio modi exercitationem natus.</p>
+              <p><strong>Super eroi della customer care</strong></p>
+              <p className='values-subtitle'>Faremo in modo che il tuo sito sia sempre funzionante, con il menù aggiornato e con foto di qualità e quando avrai bisogno di aiuto noi ci saremo!</p>
             </div>
           </div>
         </div>
