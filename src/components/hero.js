036d45ecb28f2abf8bd850c234c512cfb3eea5d0
Guido <carucciguido@gmail.com>
2020-09-08 10:41:50 +0200

    fix video home

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Cta from './cta'
 import './hero.css'
-import ReactPlayer from 'react-player'
+// import ReactPlayer from 'react-player'
 
 export default ({ linkGetStarted, textGetStarted, heroSubtitle1, heroSubtitle2, ctaSubtitle }) => {
   return (
@@ -22,9 +22,10 @@ export default ({ linkGetStarted, textGetStarted, heroSubtitle1, heroSubtitle2, 
           </p>
         </div>
         <div className='video-container' id='video-desktop'>
-          <ReactPlayer
+          {/* <ReactPlayer
             url='https://www.youtube.com/watch?v=Z_12MmcTHig&feature=youtu.be'
-          />
+          /> */}
+          <iframe width='560' height='315' src='https://www.youtube.com/embed/Z_12MmcTHig' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen />
         </div>
         <div class='video-container' id='video-mobile'>
           {/* <iframe width='560' height='315' src='https://www.youtube.com/watch?v=Z_12MmcTHig&feature=youtu.be' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen /> */}
