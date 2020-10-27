14d9b23f851d8226813c1ff6c7717efccb6997db
Guido <carucciguido@gmail.com>
2020-10-27 10:04:50 +0000

    hubspot logo

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,41 +1,77 @@
-// import React from 'react'
-// import { graphql } from 'gatsby'
-// import Img from 'gatsby-image'
-// import Seo from '../components/seo'
-// import Layout from '../components/layout'
-// import FooterCta from '../components/footerCta'
-// import './blog-post.css'
+import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import Seo from '../components/seo'
+import Layout from '../components/layout'
+import FooterCta from '../components/footerCta'
+import './blog-post.css'
 
-// const excapeSpecialHtmlCharacter = string => {
-//   return string
-//     .replace(/&amp;/g, '&')
-//     .replace(/&lt;/g, '<')
-//     .replace(/&gt;/g, '>')
-//     .replace(/&quot;/g, '"')
-//     .replace(/&#039;/g, "'")
-// }
+const excapeSpecialHtmlCharacter = string => {
+  return string
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'")
+}
 
-// const BlogTemplate = ({ pageContext: { locale }, data }) => {
-//   const post = data.wordpressPost
-//   return (
-//     <Layout locale={locale} data={data}>
-//       <Seo title={post.yoast_title} description={excapeSpecialHtmlCharacter(post.yoast_meta[0].content)} />
-//       {post.featured_media && <Img fluid={post.featured_media.localFile.childImageSharp.fluid} />}
-//       <div
-//         className='post'
-//         dangerouslySetInnerHTML={{ __html: post.content }}
-//       />
-//       <FooterCta
-//         linkGetStarted={data.header.childHeaderJson.linkGetStarted}
-//         textGetStarted={data.header.childHeaderJson.textGetStarted}
-//         title={data.footerCta.childFooterCtaJson.title}
-//         subtitle={data.footerCta.childFooterCtaJson.subtitle}
-//       />
-//     </Layout>
-//   )
-// }
+const BlogTemplate = ({ pageContext: { locale }, data }) => {
+  const post = data.wordpressPost
+  return (
+    <Layout locale={locale} data={data}>
+      <Seo title={post.yoast_title} description={excapeSpecialHtmlCharacter(post.yoast_meta[0].content)} />
+      {/* {post.featured_media && <Img fluid={post.featured_media.localFile.childImageSharp.fluid} />} */}
+      <div
+        className='post'
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      />
+      <FooterCta
+        linkGetStarted={data.header.childHeaderJson.linkGetStarted}
+        textGetStarted={data.header.childHeaderJson.textGetStarted}
+        title={data.footerCta.childFooterCtaJson.title}
+        subtitle={data.footerCta.childFooterCtaJson.subtitle}
+      />
+    </Layout>
+  )
+}
 
-// export default BlogTemplate
+export default BlogTemplate
+
+export const query = graphql`
+  query currentPostQuery($id: String!) {
+    header: file(name: { eq: "it" }, relativeDirectory: { eq: "header" }) {
+      childHeaderJson {
+        home
+        homeLink
+        about
+        aboutLink
+        services
+        contact
+        contactLink
+        linkGetStarted
+        textGetStarted
+      }
+    }
+    footerCta: file(
+      name: { eq: "it" }
+      relativeDirectory: { eq: "footer-cta" }
+    ) {
+      childFooterCtaJson {
+        title
+        subtitle
+      }
+    }
+    wordpressPost(id: { eq: $id }) {
+      title
+      content
+      yoast_title
+      yoast_meta{
+        property
+        content
+      }
+    }
+  }
+`
 
 // export const query = graphql`
 //   query currentPostQuery($id: String!) {
